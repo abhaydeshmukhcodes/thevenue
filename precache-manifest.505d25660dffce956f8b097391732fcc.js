a0be35aed962f07ee5d7e9f6ee435b128fa33dac
Abhay Deshmukh <deshmukhabhayvilas@gmail.com>
2019-05-24 12:11:17 +0530

    Updates

---
+++ precache-manifest.505d25660dffce956f8b097391732fcc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "7bfa7c08fcfca010c5de",
+    "url": "/thevenue/static/css/main.b6121231.chunk.css"
+  },
+  {
+    "revision": "7bfa7c08fcfca010c5de",
+    "url": "/thevenue/static/js/main.dd73a0f0.chunk.js"
+  },
+  {
+    "revision": "2dfd02fe05d7b2c52a81",
+    "url": "/thevenue/static/js/runtime~main.1a802491.js"
+  },
+  {
+    "revision": "651074bc7d69c2a0f7d0",
+    "url": "/thevenue/static/js/2.ac5e1fe2.chunk.js"
+  },
+  {
+    "revision": "84753208e3ace6c36a3a29c731983a69",
+    "url": "/thevenue/static/media/slide_one.84753208.jpg"
+  },
+  {
+    "revision": "3a3ffd3696fea8999087bbdcf97d69d2",
+    "url": "/thevenue/static/media/slide_two.3a3ffd36.jpg"
+  },
+  {
+    "revision": "9aa09a50b7eb84e96d522c10dfaa78e5",
+    "url": "/thevenue/static/media/slide_three.9aa09a50.jpg"
+  },
+  {
+    "revision": "0bf39e96fa495f5f9b6c1ff5781803dd",
+    "url": "/thevenue/index.html"
+  }
+];
